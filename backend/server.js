745e95c7be072f2fb492569c3825f16581f1b110
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2021-10-12 15:40:51 -0400

    [AP-7] Initial express server commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+/*=========================
+
+MAIN ENTRY POINT TO EXPRESS SERVER
+
+This file is where we create the server, set up the routes,
+set up the middleware and all other configs our server will
+have. The file should finish by running the server and listening
+on a certain PORT number.
+
+=========================*/
+const express = require('express');
+const dotenv = require('dotenv');
+
+// load in environment variables from config.env
+// this lets us access env. variables by using proccess.env.[VARIABLE_NAME]
+dotenv.config({ path: './api_config/config.env' });
+
+// set up PORT number from env. variables, 5000 by default
+const PORT = process.env.PORT || 5000;
+
+// create the server app
+const app = express();
+
+// launch server app by listening on the PORT
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+})
