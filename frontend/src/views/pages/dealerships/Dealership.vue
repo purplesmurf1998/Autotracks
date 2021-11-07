bb71028e38e29ae963a642442bf345d514bb522f
Adrien Kamran <adrien.kamran@gmail.com>
2021-11-07 00:40:42 -0400

    [AP-68] Viewing dealerships for currently logged-in admin is now possible
    Progress:
    - Click the "Dealerships" panel on the left navbar to see the full list of dealerships created by the currently logged-in admin user.
    - Click any row to see details on the selected dealership. Click the blue return button to be brought back to the table page.
    
    Bugs to fix:
    - Router for the "details" route needs to be removed.
    - Admin "first_name" field does not display correctly, something about scoped variables not being visible...
    
    Nota Bene:
    This admin user is "John Appleseed". Use john.appleseed@netscape.net to log in with password123.

--- frontend/src/views/pages/dealerships/Dealership.vue
+++ frontend/src/views/pages/dealerships/Dealership.vue
@@ -1,13 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <CCol>
+      <CCard>
+        <!--        <CCardImg-->
+        <!--          variant="top"-->
+        <!--          width="5%"-->
+        <!--          src="https://www.fireglass.com/resources/design-ideas/case-studies/2018/audi-dealership/img/Audi.jpg"-->
+        <!--        />-->
+        <CCardBody>
+          <h1>{{dealership.name}}</h1>
+          <h2>{{dealership.admin.first_name}}'s dealership</h2>
+          <p>{{dealership.description}}</p>
+          <CButton
+            color="primary"
+            @click="resetSelectedDealership"
+          >
+            Hide details
+          </CButton>
+        </CCardBody>
+        <CCardFooter>
+          <small class="text-muted">
+            {{dealership.created_at}}
+          </small>
+        </CCardFooter>
+      </CCard>
+    </CCol>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Dealership',
-    data() {
-        return {};
-    }
+  name: 'Dealership',
+  data() {
+      return {};
+  },
+  props:[
+    "dealership",
+    "resetSelectedDealership"
+  ]
+
 }
 </script>
 
