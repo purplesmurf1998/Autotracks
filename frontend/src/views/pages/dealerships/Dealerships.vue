968784ded495e70c1d4c76490da677d3d48d8c4a
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2021-11-07 05:56:16 -0500

    Rebasing sprint-3-frontend merge conflict

--- frontend/src/views/pages/dealerships/Dealerships.vue
+++ frontend/src/views/pages/dealerships/Dealerships.vue
@@ -1,13 +1,113 @@
 <template>
-  
+  <div>
+    <CRow v-if="!selectedDealership">
+      <CCol>
+        <CCard>
+          <CCardHeader>
+            <slot name="header">
+              List of registered dealerships
+            </slot>
+          </CCardHeader>
+          <CCardBody>
+            <CDataTable
+              :fields="tableFields"
+              :items="tableItems"
+              :striped="true"
+              :items-per-page="10"
+              :fixed="true"
+              :clickable-rows="true"
+              @row-clicked="clickRow"
+            >
+              <template #admin="{item}">
+                <td>
+                  {{item.admin.first_name}}
+                </td>
+              </template>
+            </CDataTable>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+    <CRow>
+      <Dealership
+        v-if="selectedDealership != null"
+        :dealership="selectedDealership"
+        :resetSelectedDealership="resetSelectedDealership"
+      ></Dealership>
+    </CRow>
+  </div>
 </template>
 
 <script>
+import Dealership from './Dealership'
+const axios = require ('axios');
 export default {
-    name: 'Dealerships',
-    data() {
-        return {};
+  name: 'Dealerships',
+  components: {Dealership},
+  data() {
+    return {
+      selectedDealership: null,
+      tableItems: [
+        {
+          name: 'Audi Saint Viateur',
+          description: 'The only one that I could come up with!',
+          admin: 'Qandeel',
+          created: '1989/11/14'
+        },
+        {
+          name: 'Audi Saint Viateur',
+          description: 'The only one that I could come up with!',
+          admin: 'Qandeel',
+          created: '1989/11/14'
+        },
+        {
+          name: 'Audi Saint Viateur',
+          description: 'The only one that I could come up with!',
+          admin: 'Qandeel',
+          created: '1989/11/14'
+        }
+      ],
+      tableFields: [
+        {key: 'name'},
+        {key: 'description'},
+        {key: 'admin.first_name'},
+        {key: 'created_at'}
+      ],
+      dName: '[Audi Saint Viateur]',
+      dDescription: '[The only one that I could come up with!]',
+      dAdmin: '[Qandeel]',
+      dCreated: '[1989/11/14]'
     }
+  },
+  mounted() {
+    this.fetchDealerships()
+  },
+  methods: {
+    clickRow(dealership) {
+      this.selectedDealership = dealership
+      console.log(dealership)
+    },
+    resetSelectedDealership() {
+      this.selectedDealership = null
+      console.log("Selected Dealership has been reset")
+    },
+    fetchDealerships() {
+      console.log(this.$store.state.auth.userId + "\n" + this.$store.state.auth.token)
+
+      axios({
+        method: 'GET',
+        url: 'http://localhost:5000/api/v1/dealerships/?admin='+this.$store.state.auth.userId+"&populate=admin",
+        headers:{
+          'Authorization':'Bearer '+this.$store.state.auth.token
+        }
+      }).then(response => {
+        this.tableItems = response.data.data
+        console.log(response)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
