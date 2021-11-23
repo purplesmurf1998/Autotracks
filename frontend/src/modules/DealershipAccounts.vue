a38870aee58b87918c7edef07ed977d0f2d835ed
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2021-11-23 11:33:06 -0500

    [AP-79] Refactoring Type: Replace inline code with function call
    Moved the staff account logic into a separate DealershipAccounts.vue component from the Dealership.vue component to make the Dealership.vue component more readable and manageable

---
+++ frontend/src/modules/DealershipAccounts.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <CCard class="mt-2">
+      <CCardBody>
+        <CRow>
+          <CCol>
+            <h3>List of staff accounts</h3>
+            <CListGroup class="mt-3" v-if="staff.length > 0">
+              <CListGroupItem
+                tag="button"
+                v-for="(user, index) in staff"
+                :key="user._id"
+                :id="user._id"
+                :active="
+                  selectedStaffAccount && user._id == selectedStaffAccount
+                "
+                @click="setActiveStaff(user, index)"
+              >
+                {{ user.first_name }} {{ user.last_name }}
+              </CListGroupItem>
+            </CListGroup>
+            <CButton
+              color="primary"
+              class="mt-2"
+              id="create-staff-acc"
+              @click="
+                () => {
+                  addingStaffAccount = false;
+                  editingUser = false;
+                  addingStaffAccount = true;
+                }
+              "
+            >
+              Create a staff account
+            </CButton>
+          </CCol>
+          <CCol>
+            <h3>Selected staff account details</h3>
+            <user-card
+              class="mt-2"
+              :user="selectedStaffAccount"
+              :editingUser="editingUser"
+              :setEditingUser="setEditingUser"
+              :updateUser="updateUser"
+              :index="selectedStaffIndex"
+              v-if="!!selectedStaffAccount"
+            />
+          </CCol>
+        </CRow>
+      </CCardBody>
+    </CCard>
+    <CModal :show.sync="addingStaffAccount" :centered="true" size="lg">
+      <staff-account-add
+        v-if="addingStaffAccount"
+        :setAddingStaffAccount="setAddingStaffAccount"
+        :addNewStaffAccount="addNewStaffAccount"
+      />
+      <template #header>
+        <h6 class="modal-title">Add a staff account to your dealership!</h6>
+        <CButtonClose @click="addingStaffAccount = false" />
+      </template>
+      <template #footer>
+        <span></span>
+      </template>
+    </CModal>
+  </div>
+</template>
+
+<script>
+import StaffAccountAdd from "./StaffAccountAdd.vue";
+import UserCard from "./UserCard.vue";
+const axios = require('axios');
+
+export default {
+  name: 'DealershipAccounts',
+  data() {
+    return {
+      staff: [],
+      selectedStaffAccount: null,
+      selectedStaffIndex: null,
+      addingStaffAccount: false,
+      editingUser: false,
+    }
+  },
+  methods: {
+    setActiveStaff(user, index) {
+      this.selectedStaffAccount = user;
+      this.selectedStaffIndex = index;
+      this.setEditingUser(false);
+    },
+    setEditingUser(value) {
+      this.editingUser = value;
+    },
+    setAddingStaffAccount(value) {
+      this.addingStaffAccount = value;
+    },
+    updateUser(newUser, index) {
+      let newStaff = this.staff;
+      newStaff[index] = newUser;
+      this.staff = newStaff;
+      this.selectedStaffAccount = newUser;
+    },
+    addNewStaffAccount(user) {
+      let newStaff = this.staff;
+      newStaff.push(user);
+      this.staff = newStaff;
+    },
+  },
+  beforeCreate() {
+    // fetch dealership staff users
+    axios({
+      method: "GET",
+      url: `http://localhost:5000/api/v1/users/?dealership=${this.$route.params.dealershipId}`,
+      headers: {
+        Authorization: `Bearer ${this.$store.state.auth.token}`,
+      },
+    })
+    .then((response) => {
+      if (response.data.success) {
+        this.staff = response.data.data;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  },
+  components: {
+    'staff-account-add': StaffAccountAdd,
+    'user-card': UserCard
+  }
+}
+</script>
--- frontend/src/views/pages/dealerships/Dealership.vue
+++ frontend/src/views/pages/dealerships/Dealership.vue
@@ -2,11 +2,6 @@
   <div>
     <CCol>
       <CCard>
-        <!--        <CCardImg-->
-        <!--          variant="top"-->
-        <!--          width="5%"-->
-        <!--          src="https://www.fireglass.com/resources/design-ideas/case-studies/2018/audi-dealership/img/Audi.jpg"-->
-        <!--        />-->
         <CCardBody>
           <div v-if="!editingDealership">
             <h1>{{ dealership.name }}</h1>
@@ -47,78 +42,15 @@
           >
         </div>
       </CRow>
-      <CCard class="mt-2">
-        <CCardBody>
-          <CRow>
-            <CCol>
-              <h3>List of staff accounts</h3>
-              <CListGroup class="mt-3" v-if="staff.length > 0">
-                <CListGroupItem
-                  tag="button"
-                  v-for="(user, index) in staff"
-                  :key="user._id"
-                  :id="user._id"
-                  :active="
-                    selectedStaffAccount && user._id == selectedStaffAccount
-                  "
-                  @click="setActiveStaff(user, index)"
-                >
-                  {{ user.first_name }} {{ user.last_name }}
-                </CListGroupItem>
-              </CListGroup>
-              <CButton
-                color="primary"
-                class="mt-2"
-                id="create-staff-acc"
-                @click="
-                  () => {
-                    addingStaffAccount = false;
-                    editingUser = false;
-                    addingStaffAccount = true;
-                  }
-                "
-              >
-                Create a staff account
-              </CButton>
-            </CCol>
-            <CCol>
-              <h3>Selected staff account details</h3>
-              <user-card
-                class="mt-2"
-                :user="selectedStaffAccount"
-                :editingUser="editingUser"
-                :setEditingUser="setEditingUser"
-                :updateUser="updateUser"
-                :index="selectedStaffIndex"
-                v-if="!!selectedStaffAccount"
-              />
-            </CCol>
-          </CRow>
-        </CCardBody>
-      </CCard>
+      <dealership-accounts />
     </CCol>
-    <CModal :show.sync="addingStaffAccount" :centered="true" size="lg">
-      <staff-account-add
-        v-if="addingStaffAccount"
-        :setAddingStaffAccount="setAddingStaffAccount"
-        :addNewStaffAccount="addNewStaffAccount"
-      />
-      <template #header>
-        <h6 class="modal-title">Add a staff account to your dealership!</h6>
-        <CButtonClose @click="addingStaffAccount = false" />
-      </template>
-      <template #footer>
-        <span></span>
-      </template>
-    </CModal>
+    
   </div>
 </template>
 
 <script>
-import StaffAccountAdd from "./StaffAccountAdd.vue";
-import UserCard from "../../../modules/UserCard.vue";
-import ViewUser from "../../../modules/ViewUser.vue";
 import EditDealership from "./EditDealership.vue";
+import DealershipAccounts from "../../../modules/DealershipAccounts.vue"
 
 const axios = require("axios");
 
@@ -127,40 +59,13 @@ export default {
   data() {
     return {
       dealership: {},
-      staff: [],
-      selectedStaffAccount: null,
-      selectedStaffIndex: null,
-      addingStaffAccount: false,
-      editingUser: false,
       editingDealership: false,
     };
   },
   methods: {
-    setActiveStaff(user, index) {
-      this.selectedStaffAccount = user;
-      this.selectedStaffIndex = index;
-      this.setEditingUser(false);
-    },
-    setEditingUser(value) {
-      this.editingUser = value;
-    },
-    setAddingStaffAccount(value) {
-      this.addingStaffAccount = value;
-    },
     setEditingDealership(value) {
       this.editingDealership = value;
-    },
-    updateUser(newUser, index) {
-      let newStaff = this.staff;
-      newStaff[index] = newUser;
-      this.staff = newStaff;
-      this.selectedStaffAccount = newUser;
-    },
-    addNewStaffAccount(user) {
-      let newStaff = this.staff;
-      newStaff.push(user);
-      this.staff = newStaff;
-    },
+    }
   },
   beforeCreate() {
     // fetch dealership details
@@ -179,29 +84,10 @@ export default {
       .catch((err) => {
         console.log(err);
       });
-
-    // fetch dealership staff users
-    axios({
-      method: "GET",
-      url: `http://localhost:5000/api/v1/users/?dealership=${this.$route.params.dealershipId}`,
-      headers: {
-        Authorization: `Bearer ${this.$store.state.auth.token}`,
-      },
-    })
-      .then((response) => {
-        if (response.data.success) {
-          this.staff = response.data.data;
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
   },
   components: {
-    "staff-account-add": StaffAccountAdd,
-    "user-card": UserCard,
     "edit-dealership": EditDealership,
-    ViewUser,
+    "dealership-accounts": DealershipAccounts
   },
 };
 </script>
