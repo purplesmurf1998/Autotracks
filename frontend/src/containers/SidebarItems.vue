0e9fbcee474e31eba776a9fdbd7496fcc4a025cf
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2021-11-27 09:27:56 -0500

    [AP-79] Refactoring Type: Remove Dead Code
    Removed the dead code in the router/index.js file. Also removed the un-used button to access the vehicle properties.

--- frontend/src/containers/SidebarItems.vue
+++ frontend/src/containers/SidebarItems.vue
@@ -40,12 +40,6 @@ export default {
             },
             {
               _name: "CSidebarNavItem",
-              name: "Vehicle Properties",
-              to: "/pages/vehicle/properties",
-              icon: "cil-pencil",
-            },
-            {
-              _name: "CSidebarNavItem",
               name: "Inventory",
               to: "/inventory",
               icon: "cil-spreadsheet",
