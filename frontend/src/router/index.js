68c126a49c7529d76a2452658cdb5918bb5b4d63
AbdulSir <abd.sirawan@hotmail.com>
2021-12-31 12:20:41 -0500

    Direct to 404page if URL is invalid

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -101,6 +101,21 @@ const Message = () => import('@/views/apps/email/Message')
 
 Vue.use(Router)
 
+const validPathNames = [
+  'Home',
+  'Dashboard',
+  'Dealerships',
+  'Dealership Details',
+  'Vehicle Inventory',
+  'Vehicle',
+  'Add Vehicle Inventory',
+  'Page404',
+  'Page500',
+  'Login',
+  'Register',
+  'Change Password'
+]
+
 const router = new Router({
   mode: 'hash', // https://router.vuejs.org/api/#mode
   linkActiveClass: 'open active',
@@ -224,9 +239,12 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
 
+  //If path name is invalid, redirect to 404 page.
+  if (!validPathNames.includes(to.name))
+    router.replace('/pages/404');
   // if the promptPasswordChange flag is true and we're not already going to the 
   // change password page, redirect to change password page
-  if (Store.state.auth.promptPasswordChange && to.path != '/pages/changePassword') {
+  else if (Store.state.auth.promptPasswordChange && to.path != '/pages/changePassword') {
     next('/pages/changePassword');
   }
 
