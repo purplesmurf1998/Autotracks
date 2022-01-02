acd6ea20bf11a50c5f991a36b6daf7db8483d537
AbdulSir <abd.sirawan@hotmail.com>
2022-01-02 00:22:36 -0500

    Updated the each test suite to sign in and grab a fresh token
    The reason for this change is because the token expires every 30 days and the tests become invalid

--- backend/tests/dealershipControllerTest.js
+++ backend/tests/dealershipControllerTest.js
@@ -10,9 +10,31 @@ chai.use(chaiHttp);
 
 let dealership_id = "";
 
-const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MThhYWNmNDVjZGM3NWI4Mjg4ZWI5YjUiLCJpYXQiOjE2MzgxMzc1ODUsImV4cCI6MTY0MDcyOTU4NX0.gv4Q-vPm-MqfHVsY7BYnBThxBI3bHZtUC5JtukHp340';
+//creds
+const email = "admin@account.com";
+const password = "password";
+let token = "";
+
 // The following tests test the entire Dealership Controller Class
 describe('Testing Dealership Controller Class', () => {
+  //The below test checks if we can sign users/admin in successfully
+  describe('Admin/User Sign In API Test', () => {
+    it('should return 200 when the admin/user is signed in', (done) => {
+      chai.request(app)
+        .post("/api/v1/auth/signin")
+        .send({
+            email,
+            password
+        })
+        .end( (err, response) => {
+          response.should.have.status(200);
+          admin_id = response.body.payload._id;
+          response.body.should.be.a('object');
+          token = response.body.token;
+          done();
+        });
+    });
+  });
 
   //The below test, checks if we can create a dealership successfully
   describe('Create Dealership API Test', () => {
@@ -25,7 +47,7 @@ describe('Testing Dealership Controller Class', () => {
             "description": "This is a simple test dealership.",
             "admin": "618aacf45cdc75b8288eb9b5"
         })
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(201);
           //We store the dealership_id that we created so we can delete it later
@@ -46,7 +68,7 @@ describe('Testing Dealership Controller Class', () => {
             "name": "Dealership #1",
             "description": "This is a simple test dealership.",
         })
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(400);
           done();
@@ -65,7 +87,7 @@ describe('Testing Dealership Controller Class', () => {
             "description": "This is a simple test dealership.",
             "admin": "718aacf45cdc75b8288eb9b5"
         })
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(404);
           done();
@@ -84,7 +106,7 @@ describe('Testing Dealership Controller Class', () => {
             "description": "This is a simple test dealership.",
             "admin": "6192c675d871bef9ad3887d1"
         })
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(400);
           done();
@@ -97,7 +119,7 @@ describe('Testing Dealership Controller Class', () => {
     it('should return 200 when the dealership is present: ' + dealership_id, (done) => {
       chai.request(app)
         .get("/api/v1/dealerships/" + dealership_id)
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(200);
           response.body.data.should.be.a('object');
@@ -113,7 +135,7 @@ describe('Testing Dealership Controller Class', () => {
     it('should return 404 when the dealership is NOT present', (done) => {
       chai.request(app)
         .get("/api/v1/dealerships/718b3bf134f07d9a91c32a1b")
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(404);
           done();
@@ -126,7 +148,7 @@ describe('Testing Dealership Controller Class', () => {
     it('should return 200 when it returns dealerships array', (done) => {
       chai.request(app)
         .get("/api/v1/dealerships")
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(200);
           response.body.payload.should.be.a('array');
@@ -146,7 +168,7 @@ describe('Testing Dealership Controller Class', () => {
         .send({
             "name": "Dealership #2",
         })
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(200);
           response.body.should.be.a('object');
@@ -165,7 +187,7 @@ describe('Testing Dealership Controller Class', () => {
         .send({
             "name": "Dealership #2",
         })
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(404);
           done();
@@ -178,7 +200,7 @@ describe('Testing Dealership Controller Class', () => {
     it('should return 200 when the dealership is deleted', (done) => {
       chai.request(app)
         .delete("/api/v1/dealerships/" + dealership_id)
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(200);
           response.body.should.be.a('object');
@@ -192,7 +214,7 @@ describe('Testing Dealership Controller Class', () => {
     it('should return 401 when the dealership is deleted', (done) => {
       chai.request(app)
         .delete("/api/v1/dealerships/718b3bf134f07d9a91c32a1b")
-        .set('authorization', token)
+        .set('authorization', 'Bearer ' + token)
         .end( (err, response) => {
           response.should.have.status(401);
           done();
