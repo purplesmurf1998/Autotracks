bd1065e9f0fbd88b1150400ed5b1b9533a05fc3e
AbdulSir <abd.sirawan@hotmail.com>
2022-01-25 20:16:49 -0500

    Removed the ability to edit vehicle props if vehicle is delivered

--- frontend/src/modules/vehicles/VehicleProperties.vue
+++ frontend/src/modules/vehicles/VehicleProperties.vue
@@ -7,7 +7,7 @@
           <CButton
             color="primary"
             @click="setEditingProperties(true)"
-            v-if="!editingProperties && userHasPermissions('Edit Vehicles')"
+            v-if="!editingProperties && userHasPermissions('Edit Vehicles') && !vehicle.delivered"
             :disabled="!!vehicle.soldBy"
           >
             Edit
