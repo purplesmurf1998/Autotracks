d0d3323a2584903486cbd0d974f4bfef4c2f0324
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2022-01-26 20:30:36 -0500

    [AP-31] Creating lists in the frontend

---
+++ frontend/src/views/pages/vehicleLists/AddVehicleList.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <CAlert show :color="messageType" v-if="message" class="mb-2">{{ message }}</CAlert>
+    <CForm @submit.prevent="submit">
+      <CInput
+        label="Title"
+        :lazy="false"
+        :value.sync="title"
+        placeholder="Custom List Title"
+      />
+
+      <p class="mb-2">Notes</p>
+      <quill-editor :content="notes"/>
+      <CRow class="justify-content-center mt-3">
+        <CButton 
+          color="primary"
+          type="submit"
+          id = "create-dealership"
+          :disabled="disableButtons"
+        >
+          Create
+        </CButton>
+        <CButton 
+          class="ml-1"
+          color="danger"
+          :disabled="disableButtons"
+        >
+          Cancel
+        </CButton>
+      </CRow>
+    </CForm>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+import Vue from 'vue'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.use(VueQuillEditor)
+
+export default {
+  props: ['finishAddingVehicleList'],
+  data() {
+    return {
+      title: '',
+      notes: '',
+      disableButtons: false,
+      message: null,
+      messageType: null,
+    }
+  },
+  methods: {
+    submit() {
+      this.disableButtons = true
+
+      const list = {
+        title: this.title,
+        notes: this.notes,
+        owner: this.$store.state.auth.userId
+      };
+
+      this.postVehicleList(list);
+    },
+    postVehicleList(data) {
+      axios({
+        method: "POST",
+        url: `${this.$store.state.api}/vehicle-list`,
+        headers: {
+          Authorization: `Bearer ${this.$store.state.auth.token}`,
+        },
+        data
+      }).then((response) => {
+        this.disableButtons = false;
+        this.title = '';
+        this.notes = '';
+        this.finishAddingVehicleList(response.data.payload);
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+    showMessage(message, messageType) {
+      this.message = message;
+      this.messageType = messageType;
+      setTimeout(() => {
+        this.message = null;
+        this.messageType = null;
+      }, 5000);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/pages/vehicleLists/UserVehicleLists.vue
+++ frontend/src/views/pages/vehicleLists/UserVehicleLists.vue
@@ -5,22 +5,53 @@
         <slot name="header">Table of custom vehicle lists</slot>
       </CCardHeader>
       <CCardBody>
+        <CRow class="d-flex justify-content-between pl-3 pr-3">
+          <CButton color="primary mb-3" @click="addingVehicleList = true">Create a custom vehicle list</CButton>
+        </CRow>
         <CDataTable
           :fields="tableFields"
           :items="tableItems"
-          :striped="true"
           :items-per-page="10"
           :fixed="true"
-          sorter
+          :sorter="true"
+          :hover="true"
           :clickable-rows="true"
           @row-clicked="clickRow"
-        />
+        >
+          <template #date_created="{item}">
+            <td>
+              {{ getFormattedDate(item.date_created) }}
+            </td>
+          </template>
+          <template #last_modified="{item}">
+            <td>
+              {{ !!item.last_modified ? getFormattedDate(item.last_modified) : "" }}
+            </td>
+          </template>
+        </CDataTable>
       </CCardBody>
     </CCard>
+    <CModal
+      :show.sync="addingVehicleList"
+      :centered="true"
+      size="lg"
+    >
+      <add-vehicle-list :finishAddingVehicleList="finishAddingVehicleList" />
+      <template #header>
+        <h6 class="modal-title">Creating a custom vehicle list!</h6>
+        <CButtonClose @click="addingVehicleList = false" />
+      </template>
+      <template #footer>
+        <span></span>
+      </template>
+    </CModal>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+import AddVehicleList from "./AddVehicleList.vue"
+
 export default {
   data() {
     return {
@@ -38,17 +69,59 @@ export default {
           label: "Last Modified"
         }
       ],
-      tableItems: []
+      tableItems: [],
+      addingVehicleList: false
     }
   },
   methods: {
-  },
-  beforeMount() {
+    getFormattedDate(date) {
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ]
 
+      const formattedDate = months[parseInt(date.substring(5, 7)) - 1] + " " + date.substring(8, 10) + ", " + date.substring(0, 4);
+      return formattedDate;
+    },
+    finishAddingVehicleList(vehicleList) {
+      this.addingVehicleList = false;
+      this.fetchUserVehicleLists();
+    },
+    fetchUserVehicleLists() {
+      axios({
+        method: "GET",
+        url: `${this.$store.state.api}/vehicle-list/user/${this.$store.state.auth.userId}`,
+        headers: {
+          Authorization: `Bearer ${this.$store.state.auth.token}`,
+        },
+      }).then((response) => {
+        this.tableItems = response.data.payload;
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+    clickRow(vehicleList) {
+      console.log(vehicleList);
+    }
+  },
+  mounted() {
+    this.fetchUserVehicleLists();
+  },
+  components: {
+    AddVehicleList
   }
 }
 </script>
 
 <style>
-
 </style>
