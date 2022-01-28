d95ebf15a3ed6b275549abbc66df9ee86d33ab9d
Adrien Kamran <adrien.kamran@gmail.com>
2022-01-28 18:20:04 -0500

    Testing changes

--- frontend/src/views/charts/DashboardLineChart.vue
+++ frontend/src/views/charts/DashboardLineChart.vue
@@ -1,6 +1,6 @@
 <template>
  <CChartLine
-   :datasets="something"
+   :datasets="this.chartData"
  />
 </template>
 
@@ -15,11 +15,21 @@ export default {
   // Data stored in the component
   data() {
     return {
+      chartData: [
+        {
+          // Dummy data, becomes overwritten
+          data: [{x: 10, y: 20}, {x: 15, y: 15}, {x: 20, y: 10}]
+        }
+      ],
+      dataPoints: [
 
+      ],
+      chartOptions: []
     };
   },
   // Methods which can be called by the component
   methods: {
+    // Request all the sales from the selected dealership
     fetchSalesFromDealership(dealership) {
       axios({
         method: "GET",
@@ -28,15 +38,21 @@ export default {
           Authorization: `Bearer ${this.$store.state.auth.token}`,
         },
       })
+        // From this point, to ensure sync, all processing methods MUST reside in this block
         .then((response) => {
           console.log("Payload below:")
           console.log(response.data.payload)
+          this.chartData = response.data.payload
+
         })
         .catch((error) => {
           console.log(error);
-          //Show an error message instead of showing the 404 page
+          // Show an error message instead of showing the 404 page
           this.$router.replace("/pages/404");
         });
+    },
+    processDataPoints() {
+
     },
   },
   // Actions to take the instant the component is loaded
