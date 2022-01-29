e6e2c3e7fc2366cb8e9c285a9e68eeef8d253812
TonyMontana11 <anth.chraim@gmail.com>
2022-01-29 15:33:02 -0500

    updated dependencies and fixed tests

--- frontend/tests/unit/views/Dashboard.spec.js
+++ frontend/tests/unit/views/Dashboard.spec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { shallowMount } from '@vue/test-utils'
 import CoreuiVue from '@coreui/vue-pro'
-import Dashboard from '@/views/Dashboard'
+import Dashboard from './../../../../coreui-template-code/src/views/Dashboard.vue'
 
 
 Vue.use(CoreuiVue)
