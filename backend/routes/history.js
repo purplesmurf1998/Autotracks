76322134b09141fafec0ee8831205f9d3fa98844
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2022-01-29 14:32:32 -0500

    Refactoring Type: Remove Dead Code
    https://refactoring.com/catalog/removeDeadCode.html

--- backend/routes/history.js
+++ backend/routes/history.js
@@ -3,15 +3,10 @@ const router = express.Router();
 
 const {
   getHistory,
-  createHistory,
-
 } = require('../controllers/historyController');
 
 // get authentication middleware
 const { protect } = require('../middleware/auth');
-
-router.route('/')
-  .post(protect, createHistory);
 
 router.route('/vehicle/:vehicleId')
   .get(protect, getHistory);
