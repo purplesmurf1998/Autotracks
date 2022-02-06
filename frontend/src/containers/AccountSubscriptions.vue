c7dcf172890bdfe29e1227567b91d33338b5a531
AbdulSir <abd.sirawan@hotmail.com>
2022-02-06 13:56:44 -0500

    Added the subscriptions modal

---
+++ frontend/src/containers/AccountSubscriptions.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <CAlert v-if="!!errorMessage" color="danger">{{ errorMessage }}</CAlert>
+    <CForm @submit.prevent="submitUser">
+      <CRow>
+        <CCol>
+          <CRow>
+            <CCol>
+              <label>Vehicle Missing</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol>
+              <label>Vehicle Found</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol>
+              <label>Vehicle Moved</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>  
+          <CRow>
+            <CCol>
+              <label>Vehicle Delivery</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>
+        </CCol>
+        <CCol>
+          <CRow>
+            <CCol>
+              <label>Vehicle Deleted</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol>
+              <label>Transaction Requests</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol>
+              <label>Transaction Approvals</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol>
+              <label>Transaction Modifications</label>
+            </CCol>
+            <CCol class="d-flex justify-content-end">
+            <!-- :checked="isPermissionSelected(permission)" -->
+              <CSwitch
+                class="mr-1"
+                color="success"
+                @change.native="toggleEventChange"
+                shape="pill"
+              />
+            </CCol>
+          </CRow>
+        </CCol>
+      </CRow>
+      <hr />
+      <CRow class="d-flex justify-content-center mt-2">
+        <CButton
+          color="success"
+          class="mr-2"
+          id="save-user-changes"
+          type="submit"
+          >Save</CButton
+        >
+        <CButton color="secondary" @click="setSubscriptionModal(false)">Cancel</CButton>
+      </CRow>
+    </CForm>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  name: 'accountSubscriptions',
+  props: [
+    "setSubscriptionModal",
+  ],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    toggleEventChange() {
+        console.log("Test");
+    }
+  },
+  mounted() {
+    //this.getSale();
+  }
+}
+</script>
--- frontend/src/containers/TheHeaderDropdownAccnt.vue
+++ frontend/src/containers/TheHeaderDropdownAccnt.vue
@@ -1,52 +1,73 @@
 <template>
-  <CDropdown
-    inNav
-    class="c-header-nav-items"
-    placement="bottom-end"
-    add-menu-classes="pt-0"
-    id = "nav-dropdown"
-  >
-    <template #toggler>
-      <CHeaderNavLink>
-        <!-- <div class="c-avatar">
-          <img
-            src="img/avatars/6.jpg"
-            class="c-avatar-img "
-          />
-        </div> -->
-        <strong>{{ $store.state.auth.firstName }} {{ $store.state.auth.lastName }}</strong>
-      </CHeaderNavLink>
-    </template>
-    <CDropdownHeader
-      tag="div"
-      class="text-center"
-      color="light"
+  <div>
+    <CDropdown
+      inNav
+      class="c-header-nav-items"
+      placement="bottom-end"
+      add-menu-classes="pt-0"
+      id = "nav-dropdown"
     >
-      <strong>Settings</strong>
-    </CDropdownHeader>
-    <CDropdownItem>
-      <CIcon name="cil-task" /> Subscriptions
-    </CDropdownItem>
-    <CDropdownItem @click="logoutUser"
-    id = "log-out"
+      <template #toggler>
+        <CHeaderNavLink>
+          <strong>{{ $store.state.auth.firstName }} {{ $store.state.auth.lastName }}</strong>
+        </CHeaderNavLink>
+      </template>
+      <CDropdownHeader
+        tag="div"
+        class="text-center"
+        color="light"
+      >
+        <strong>Settings</strong>
+      </CDropdownHeader>
+      <CDropdownItem @click="subscriptionModal = true">
+        <CIcon name="cil-task" /> Subscriptions
+      </CDropdownItem>
+      <CDropdownItem @click="logoutUser"
+      id = "log-out"
+      >
+        <CIcon name="cil-account-logout" /> Logout
+      </CDropdownItem>
+    </CDropdown>
+    <CModal
+      :show.sync="subscriptionModal"
+      :centered="true"
+      size="lg"
     >
-      <CIcon name="cil-account-logout" /> Logout
-    </CDropdownItem>
-  </CDropdown>
+      <account-subscriptions
+        v-if="subscriptionModal"
+        :setSubscriptionModal="setSubscriptionModal"
+      />
+    <template #header>
+        <h6 class="modal-title">Account Subscriptions</h6>
+        <CButtonClose @click="subscriptionModal = false" />
+      </template>
+      <template #footer>
+        <span></span>
+      </template>
+    </CModal>
+  </div>
 </template>
 
 <script>
+import AccountSubscriptions from "./AccountSubscriptions.vue"
+
 export default {
   name: 'TheHeaderDropdownAccnt',
+  components: {
+    "account-subscriptions": AccountSubscriptions,
+  },
   data () {
     return { 
-      itemsCount: 42
+      subscriptionModal: false
     }
   },
   methods: {
     logoutUser() {
       this.$store.dispatch('logout');
       this.$router.push('/pages/login');
+    },
+    setSubscriptionModal(value){
+      this.subscriptionModal = value;
     }
   }
 }
