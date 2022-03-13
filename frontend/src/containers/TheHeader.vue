0889162fed57052a4162bcf16eeac1050cc3791e
Matthew S Giancola <matthew.giancola42@gmail.com>
2022-03-13 13:19:01 -0400

    Update to comply with SonarCloud

--- frontend/src/containers/TheHeader.vue
+++ frontend/src/containers/TheHeader.vue
@@ -53,7 +53,9 @@ export default {
   name: 'TheHeader',
   data(){
     return {
-      logo: logo
+    showNotifModal: false,
+      unReadNotif: [],
+      logo: logo,
     }
   },
   components: {
@@ -62,12 +64,6 @@ export default {
     TheHeaderDropdownTasks,
     TheHeaderDropdownMssgs,
     "notif-modal": NotifModal,
-  },
-  data() {
-    return {
-      showNotifModal: false,
-      unReadNotif: [],
-    }
   },
   methods: {
     setNotifModal(value) {
